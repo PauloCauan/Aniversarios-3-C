3320474b928e56d15b67eb40455edbe7f5099306
Paulo Cauan <104834064+PauloCauan@users.noreply.github.com>
2024-05-03 13:42:47 -0300

    .

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-31T23:59:00");
+const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
 const tempoObjetivo2 = new Date("2024-07-17T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-08T00:00:00");
 const tempoObjetivo4 = new Date("2024-10-01T00:00:00");
